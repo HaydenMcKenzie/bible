c22d82f112d429e6b370a15fb2fde494f70658cb
HaydenMcKenzie <Haydendanielmckenzie@gmail.com>
2024-09-06 11:40:35 +1000

    Fixed the chapter numbers into the CSV

--- src/main/scala/org/bible/utils/RenderVerses.scala
+++ src/main/scala/org/bible/utils/RenderVerses.scala
@@ -7,21 +7,33 @@ object RenderVerses {
   def renderVerses(verses: List[Verse]): String = {
     val result = new StringBuilder
     var lastTitle = ""
+    var lastChapter = -1
 
     verses.foreach { verse =>
       // If the section title changes, append the new title
       if (verse.sectionTitle != lastTitle) {
         if (result.nonEmpty) {
-          result.append("\n\n") // For paragraph break
+          result.append("\n\n") // For paragraph break before new section
         }
         result.append(s"${verse.sectionTitle}\n\n")
         lastTitle = verse.sectionTitle
       }
-      if (verse.isNewParagraph && result.nonEmpty) {
-        result.append("\n\n") // For paragraph break
+
+      // If the chapter changes, display the chapter number instead of verse number for verse 1
+      if (verse.chapter != lastChapter) {
+        if (result.nonEmpty) {
+          result.append("\n\n") // Separate chapters with new line
+        }
+        result.append(s"${verse.chapter} ${verse.verse.replace("\"", "")} ") // Chapter number instead of verse number
+        lastChapter = verse.chapter
+      } else {
+        // Append new paragraph if required
+        if (verse.isNewParagraph) {
+          if (result.nonEmpty) result.append("\n\n") // For paragraph break
+        }
+        // Append the verse number and then the verse text, removing double quotes
+        result.append(s"${verse.number} ${verse.verse.replace("\"", "")} ")
       }
-      // Append the verse number and then the verse text, removing double quotes
-      result.append(s"${verse.number} ${verse.verse.replace("\"", "")} ")
     }
 
     // Return the accumulated verses as a single string
